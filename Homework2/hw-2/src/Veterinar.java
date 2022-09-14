c7341ffae83cfb2f38927d058cc9631c7392243f
TerraIcarus <cereper1@gmail.com>
2022-09-14 19:11:12 +0300

    <-- zadanie 4

--- Homework2/hw-2/src/Veterinar.java
+++
@@ -1,6 +0,0 @@
-public class Veterinar {
-    public static void TreatAnimal(Animal animal)
-    {
-        System.out.printf("Статус: еда = %d местоположение: %s %n", animal.getFood(), animal.getLocation(), animal.isSleep());
-    }
-}
